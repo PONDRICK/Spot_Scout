ef43c312d3af29c0335b42614501f667342d3823
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-06-11 20:19:44 +0700

    Add Logout

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,35 @@
-// src/app/components/dashboard/dashboard.component.ts
+// frontend/src/app/components/dashboard/dashboard.component.ts
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../../api.service';
+import { NgModel, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-  // Logic for dashboard
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  logout() {
+    const refreshToken = localStorage.getItem('refresh_token');
+    if (refreshToken) {
+      this.apiService.logoutUser(refreshToken).subscribe(
+        (response) => {
+          console.log('Logout successful', response);
+          localStorage.removeItem('access_token');
+          localStorage.removeItem('refresh_token');
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Logout failed', error);
+        }
+      );
+    }
+  }
 }
