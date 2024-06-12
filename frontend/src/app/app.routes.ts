7a3e4f618ef57c83ae88f903ff273150e8005a82
SpiritJom <suwannop2545@gmail.com>
2024-06-12 15:58:10 +0700

    add map & get lat lon

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
+// frontend/src/app.routes.ts
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { OTPVerificationComponent } from './components/otp-verification/otp-verification.component';
-import { AuthGuard } from './auth.guard'; // Import AuthGuard
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   {
@@ -22,7 +23,7 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [AuthGuard], // Add AuthGuard to canActivate property
+    canActivate: [AuthGuard],
   },
   {
     path: 'verify-otp',
