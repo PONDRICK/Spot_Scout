805cbb146016112f3dbf51b425a8cde84f21be07
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-06-13 14:48:28 +0700

    delete social accounts and add location folder

--- spotscout/spotscout/settings.py
+++ spotscout/spotscout/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'social_accounts',
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
