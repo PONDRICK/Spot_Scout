a57b390663567e5296c083e2013a1f2038884d9a
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-06-18 19:19:03 +0700

    change number to word

--- spotscout/location/utils.py
+++ spotscout/location/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67c3a228b6f8106637af00b608f650123e1a6e078866813bcc6337eb9cbc78cf
-size 13129
+oid sha256:1fab0ea5eb76b3d8a0c60face398e039150eb48c8b2ded70071eb4b02a1756db
+size 13435
