b6225f4d58cc8dafc9f68f2017ce606eb1159a2c
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-06-18 18:36:47 +0700

    add predict model

---
+++ spotscout/location/migrations/0003_userlocation_predicted_amenity_category.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd46733af10756e28d25bf5b06cda0bf4d411a6aa89e5b88805b1ee177ba6623
+size 447
--- spotscout/location/serializers.py
+++ spotscout/location/serializers.py
@@ -1,26 +1,3 @@
-from rest_framework import serializers
-from .models import UserLocation
-
-class UserLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserLocation
-        fields = [
-            'id', 'user', 'lat', 'lon', 'province', 'ISO3166_2',
-            'distance_nearest_bank', 'count_bank_within_500m',
-            'distance_nearest_fuel', 'count_fuel_within_500m',
-            'distance_nearest_office', 'count_office_within_500m',
-            'distance_nearest_police', 'count_police_within_500m',
-            'distance_nearest_townhall', 'count_townhall_within_500m',
-            'distance_nearest_bus_station', 'count_bus_station_within_500m',
-            'distance_nearest_bus_stop', 'count_bus_stop_within_500m',
-            'distance_nearest_convenience', 'count_convenience_within_500m',
-            'distance_nearest_mall', 'count_mall_within_500m',
-            'distance_nearest_supermarket', 'count_supermarket_within_500m',
-            'distance_nearest_books', 'count_books_within_500m',
-            'distance_nearest_coffee', 'count_coffee_within_500m',
-            'distance_nearest_department_store', 'count_department_store_within_500m',
-            'distance_nearest_clothes', 'count_clothes_within_500m',
-            'distance_nearest_bakery', 'count_bakery_within_500m',
-            'distance_nearest_cafe', 'count_cafe_within_500m',
-            'population'
-        ]
+version https://git-lfs.github.com/spec/v1
+oid sha256:fbe27b2cee866ccddc87a8a6f2dc23f523b968aa3cc97698f54f851bce09b227
+size 3466
