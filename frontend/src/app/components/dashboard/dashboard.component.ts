e1a3beaaf87ab55b982b63fe4ac8011425767d74
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-06-19 20:28:02 +0700

    add comment

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:293084c56a88d2dbcc756c77dea4c7141f65c6455762bf240612465e0b059f71
-size 8427
+oid sha256:71b7dd571f94bf47c4188654d10fd74392d9a4b08d9d70e1e654f3c5d0bf3208
+size 9096
