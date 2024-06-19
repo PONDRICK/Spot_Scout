a04fec621cebff4aefe94d7b75063cc40b66ada6
SpiritJom <suwannop2545@gmail.com>
2024-06-19 18:41:23 +0700

    add function find nearest place

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-  purge: [
-    './src/**/*.{html,ts}',
-  ],
-  darkMode: false, 
-  theme: {
-    extend: {
-      colors: {
-        "custom-blue": "#1E40AF",
-        "text-red-500": "#E1341E",
-        "text-green-500": "#63cf30",
-        "ring-blue-500": "##F40B59",
-        "text-gray-700": "#888888",
-        "border-gray-300": "#575757",
-        "bg-blue-600":"#7676D3",
-        "text-white":"#F9F9F9",
-        "bg-blue-500":"#4156BE",
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
-      }, 
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c69e2c25b4d110f2bfa70ca2d4814d40e87be61c4a6ed685c9c5e951d4acd6a3
+size 1354
--- spotscout/location/urls.py
+++ spotscout/location/urls.py
@@ -1,6 +1,3 @@
-from django.urls import path
-from .views import AddUserLocationView
-
-urlpatterns = [
-    path('add-location/', AddUserLocationView.as_view(), name='add-location'),
-]
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c725f1f4b05a931add17c17cc1dde091b477458006e6ee4f34fa97168d7eb0e
+size 272
