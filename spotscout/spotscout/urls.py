bb3695b9396865c8ed8b3350cd17cbe3908e2ca9
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-06-25 18:05:59 +0700

    add admin page

--- spotscout/spotscout/urls.py
+++ spotscout/spotscout/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('accounts.urls')),
     path('api/v1/location/', include('location.urls')),
+    path('api/v1/admin/', include('accounts.urls')),
 ]
