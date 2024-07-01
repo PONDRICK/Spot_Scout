61047854b7e7bb1e1e6b766ac589df73c4cdb22b
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-07-01 14:18:22 +0700

    add search in admin

--- spotscout/spotscout/settings.py
+++ spotscout/spotscout/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     #"ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),  
     #"REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),  
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),  
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),  
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1), 
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ROTATE_REFRESH_TOKENS": True,  # Ensure tokens are rotated
